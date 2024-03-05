3f3e6d44ac8bf56319cb6c5407e1df62f786e13d
Jmarkzcod8r <jmgutierrez122091@gmail.com>
2024-03-06 01:51:23 +0800

    first commit

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,7 +1,9 @@
-import { searchCars } from '../../lib/redis';
+import { searchCars } from "../../lib/redis";
 
 export default async function handler(req, res) {
+  console.log('req query',req.query.q)
   const q = req.query.q;
   const cars = await searchCars(q);
-  res.status(200).json({ cars });
+  // res.status(200).json({ cars });
+  res.send({ cars });
 }
